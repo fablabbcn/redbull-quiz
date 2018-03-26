c7f340caab863bdb91d52c2d7553442e80a624c4
Viktor Smari <viktorsmari@gmail.com>
2018-03-26 16:42:07 +0200

    Tests were not working

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+jest.mock("react-ga")
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
