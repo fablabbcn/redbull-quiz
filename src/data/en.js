6404194bb81b3162faf4d7e3b83218eef1456273
Viktor Smari <viktorsmari@gmail.com>
2018-04-03 18:21:30 +0200

    Add tips on how to reduce exposure

--- src/data/en.js
+++ src/data/en.js
@@ -6,9 +6,10 @@ export default [
       'I live in a house near a quiet lane'
     ],
     results: [
-      'Pollution is typically higher near busy highways since traffic is a major source of air pollution. 50% of vehicle emissions can also travel a distance of up to 150 meters from their source.  Consider planting a hedge row or asking your landlord to do it. A hedge row could act as a green barrier between you and vehicle emissions, this will help to reduce your exposure to air pollution on a daily basis.',
+      'Pollution is typically higher near busy highways since traffic is a major source of air pollution. 50% of vehicle emissions can also travel a distance of up to 150 meters from their source. Consider planting a hedge row or asking your landlord to do it. A hedge row could act as a green barrier between you and vehicle emissions, this will help to reduce your exposure to air pollution on a daily basis.',
       'Great! Pollution is typically lower away from busy roads since traffic is a major source of air pollution.'
     ],
+    tip: 'Consider planting a hedge row or asking your landlord to do it.',
     images:[
       'House-near-busy-highway.png',
       'House-near-quiet-lane.png'
@@ -25,6 +26,7 @@ export default [
       'Unfortunately pollution exposure is typically higher on these roads compared to roads with hedges. This is because it has fewer hedges along the roadside. Hedges can help to reduce air pollution by up to 60%. If you want to try and reduce your exposure to air pollution whilst travelling around Guildford, try to walk down roads with hedges and trees next time you are out and about. ',
       'Great!  Walking down a road behind hedges exposes you to a lot less pollution than walking down a road where the pavement is close to traffic. This is because the hedges at the side of the road can help to reduce air pollution exposure by up to to 60%. '
     ],
+    tip: 'Try to walk down roads with hedges and trees next time you are out and about. ',
     images:[
       'Stoke-road-no-hedges.png',
       'Park-road-with-hedges.png'
@@ -41,6 +43,7 @@ export default [
       'Pollution is typically higher at the road edge but 20% of vehicle emissions reduce within 10 meters back from the curb.  If you want to try and reduce your exposure to air pollution stand as far away from the curb as possible.  ',
       'Great!  Pollution is typically higher at the road edge but 20% of vehicle emissions reduce within 10 meters back from the curb.  By standing as far back as you can from the edge of the road you can reduce your exposure to air pollution.  '
     ],
+    tip: 'Try to stand as far away from the curb as possible.',
     images:[
       'Waiting-next-to-curb.png',
       'Waiting-few-meters-from-curb.png'
@@ -57,6 +60,7 @@ export default [
       'Unfortunately you are likely to be exposed to higher levels of air pollution at this time of day. Air pollution is highest during the morning rush hours between 7-10am and evening rush hours between 4-7pm. If you can manage to travel during off-peak hours instead (10 am - 4 pm) it will not only reduce you exposure to pollution but your journey time too!',
       'Well done! Given a choice, commuting during the off-peak hours (10am - 4pm) could help reduce journey time and pollution exposure.'
     ],
+    tip: 'Try to travel during off-peak hours',
     images:[
       'Cycling-at-rush-hour.png',
       'Cycling-off-peak.png'
@@ -73,6 +77,7 @@ export default [
       'Unfortunately this is not such a great idea. Leaving your car window open lets in a lot of harmful pollutants that can cause respiratory problems.',
       'Closing your windows reduces your exposure to air pollution. You should also change the air conditioning settings to reduce external intake and instead recirculate the air inside the car. '
     ],
+    tip: 'Try closing your car window',
     images:[
       'Car-window-open.png',
       'Car-window-closed.png'
@@ -86,9 +91,10 @@ export default [
       'I always switch the motor off when stationary, even if it’s only briefly'
     ],
     results: [
-      'An idling engine can produce up to twice as many exhaust emissions as an engine in motion.  If you want to try and reduce your emissions, switch of the motor while you wait.  ',
+      'An idling engine can produce up to twice as many exhaust emissions as an engine in motion.  If you want to try and reduce your emissions, switch off the motor while you wait.  ',
       'Great!  An idling engine can produce up to twice as many exhaust emissions as an engine in motion, so it’s best to switch it off when stopped.'
     ],
+    tip: 'Switch off the motor while you wait',
     images:[
       'Idling-car.png',
       'Non-Idling-car.png'
@@ -105,6 +111,7 @@ export default [
       'That’s not quite right - even though Guildford is in relatively close proximity to both Heathrow and Gatwick airport, the main cause of pollution is the amount of traffic in and around the city centre.',
       'Correct! Even though Guildford is in relatively close proximity to both Heathrow and Gatwick airport, the main cause of pollution is the amount of traffic in and around the city centre.'
     ],
+    tip: '',
     images:[
       'Airport.png',
       'Traffic.png'
@@ -121,6 +128,7 @@ export default [
       'Unfortunately that’s not correct. Although, particulate matter includes those particles found in visible air pollution such as smog, some types of PM are much smaller than a human hair and are invisible to the naked eye. They come from a number of natural and manmade sources including road dust, construction sites and burning wood or fossil fuels. These particles can be harmful to people and cause serious health problems. Particles less than 10 micrometers in diameter (x% of a human hair) pose the greatest problems, because they can get deep into your lungs, and some may even get into your bloodstream…',
       'Well done! Some types of Particulate Matter are much smaller than a human hair and are invisible to the naked eye. They come from a number of natural and manmade sources including road dust, construction sites and burning wood or fossil fuels. These particles can be harmful to people and cause serious health problems. Particles less than 10 micrometers in diameter (x% of a human hair) pose the greatest problems, because they can get deep into your lungs, and some may even get into your bloodstream… '
     ],
+    tip: '',
     images:[
       'PM-visible-in-smog.png',
       'PM-smaller-than-hair.png'
